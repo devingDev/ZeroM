80f82bc1c67412bbe464588acb8ec8ec092798b8
devingDev <bizzy933@gmail.com>
2024-09-28 01:50:28 +0200

    make release pre

---
+++ scripts/makerelease.sh
@@ -0,0 +1,19 @@
+
+
+mkdir -p tmp/zerom_release/ux0/zerom
+mkdir -p tmp/zerom_release/ur0/tai
+
+cp ../ZeroM-base/build/ZeroM.suprx ./tmp/zerom_release/ur0/tai/
+cp ../ZeroM-base/sample/plugin/build/mygamemod.suprx ./tmp/zerom_release/ux0/zerom/
+
+cp ../INSTALL.txt ./tmp/zerom_release/
+
+cd ./tmp/
+
+zip -r zerom_release.zip ./zerom_release
+
+cd ..
+
+
+
+#rm -rf tmp
